bc6ca1502683ab1e061d6793fb6cdd8acc6a0f86
YujinB <yujinb0201@gmail.com>
2025-06-16 14:37:47 +0900

    add _redirects file
    add _redirects file

--- readytojouney/src/App.js
+++ readytojouney/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { DndProvider } from 'react-dnd';
 import Home from "./pages/Home"
